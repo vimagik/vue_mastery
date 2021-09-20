8bcbe0057418f2a0b3cc4114ebbcf11709090809
Rzhavin Georgii <rzhavin@my.com>
2021-09-20 13:13:24 +0300

    lesson 2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainComp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainComp from "@/components/MainComp";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainComp
   }
 }
 </script>
